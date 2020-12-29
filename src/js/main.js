b55c5954c22550034277730cca6efdaad280089f
Abdur Razzak Romy <41229666+Razzakromy@users.noreply.github.com>
2020-12-29 21:35:40 +0600

    Update Testimonial Section

--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,13 @@
-$(document).ready(function(){
-    $('.slider').slick({
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        autoplay: true,
-        autoplaySpeed: 3000,
-    });
+$(document).ready(function () {
+  $(".slider").slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 3000,
+    dots: true,
+    infinite: true,
+    speed: 500,
+    fade: true,
+    cssEase: "linear",
   });
+});
